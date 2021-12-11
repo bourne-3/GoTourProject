f327d99ff69523959ba4b881fd57bdb68c4c36d8
bourne-3 <595962708@qq.com>
2021-12-11 16:06:40 +0800

    practice

--- go-programming-tour-book/tour/cmd/root.go
+++ go-programming-tour-book/tour/cmd/root.go
@@ -3,16 +3,15 @@ package cmd
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use: "",
+	Use:   "",
 	Short: "",
-	Long: "",
+	Long:  "",
 }
+
 func Execute() error {
 	return rootCmd.Execute()
 }
 func init() {
 	rootCmd.AddCommand(wordCmd)
+	rootCmd.AddCommand(timeCmd)
 }
-
-
-
---
+++ go-programming-tour-book/tour/internal/timer/time.go
@@ -0,0 +1,17 @@
+package timer
+
+import "time"
+
+func GetNowTime() time.Time {
+	return time.Now()
+}
+
+// 时间推算
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	// 解析出时间
+	duration, err := time.ParseDuration(d)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return currentTimer.Add(duration), nil
+}
