b891efaea1e0ea1b006ea53618064aa4de1fd73c
bourne-3 <595962708@qq.com>
2021-12-11 22:21:01 +0800

    practice

--- go-programming-tour-book/blog-service/main.go
+++ go-programming-tour-book/blog-service/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/go-programming-tour-book/blog-service/internal/routers"
+	"net/http"
+	"time"
+)
 
 func main() {
-	r := gin.Default()
-	// 路由注册
-	r.GET("/index", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "hello, world"})
-	})
-	r.Run()
+	//r := gin.Default()
+	//// 路由注册
+	//r.GET("/index", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{"message": "hello, world"})
+	//})
+	//r.Run()
+
+	// way2
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
